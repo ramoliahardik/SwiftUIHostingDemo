27f061781261ff222ad031204b5014602963296c
ramoliahardik <49195916+ramoliahardik@users.noreply.github.com>
2020-03-16 18:33:34 +0530

    Initial Commit

---
+++ SwiftUIHostingDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftUIHostingDemo
+//
+//  Created by Hardik.Ramoliya on 16/03/20.
+//  Copyright © 2020 Hardik.Ramoliya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
